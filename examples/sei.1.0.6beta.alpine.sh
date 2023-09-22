35ef15266790d590b424ae2f8e80af8a35a416b4
gregnuj <greg@terra.money>
2023-09-22 13:59:58 -0500

    add sei

---
+++ examples/sei.1.0.6beta.alpine.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -x
+
+BASEDIR=$(dirname "$0")
+DOCKER_DIR="${BASEDIR}/.."
+IMAGE="alpine"
+NAME="sei"
+REPO="sei-protocol/sei-chain"
+TAG="1.0.6beta"
+
+cd "${DOCKER_DIR}"
+docker buildx build "." -f "${IMAGE}.Dockerfile" \
+    --load \
+    --progress plain \
+    --tag "terraformlabs/${NAME}:${TAG}" \
+    --platform "linux/amd64" \
+    --build-arg "OS=linux" \
+    --build-arg "ARCH=amd64" \
+    --build-arg "APP_NAME=${NAME}" \
+    --build-arg "BIN_NAME=${NAME}d" \
+    --build-arg "BUILD_COMMAND=make install" \
+    --build-arg "BUILD_TAGS=netgo ledger muslc" \
+    --build-arg "COSMOS_BUILD_OPTIONS=" \
+    --build-arg "GIT_TAG=${TAG}" \
+    --build-arg "GIT_REPO=${REPO}" \
+    --build-arg "GO_VERSION=1.20.8" \
+    --build-arg "MIMALLOC_VERSION=" \
+    --build-arg "LDFLAGS=-w -s -linkmode=external -extldflags \"-Wl,-z,muldefs -static\"" \
+    $@
