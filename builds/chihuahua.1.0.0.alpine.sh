db988b405ae2b5b35ccec3b4f68de895e8cd14ad
gregnuj <greg@terra.money>
2023-10-20 13:02:27 -0500

    update go versions for chihuahuia

---
+++ builds/chihuahua.1.0.0.alpine.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -x
+
+BASEDIR=$(dirname "$0")
+DOCKER_DIR="${BASEDIR}/.."
+IMAGE="alpine"
+NAME="chihuahua"
+REPO="ChihuahuaChain/chihuahua"
+TAG="1.0.0"
+GO_VERSION="1.16"
+
+cd "${DOCKER_DIR}"
+docker buildx build "." -f "${IMAGE}.Dockerfile" \
+    --load \
+    --progress plain \
+    --tag "terraformlabs/${NAME}:${TAG}" \
+    --platform "linux/amd64" \
+    --build-arg "OS=linux" \
+    --build-arg "ARCH=amd64" \
+    --build-arg "APP_NAME=${NAME}" \
+    --build-arg "BIN_NAME=${NAME}d" \
+    --build-arg "BUILD_COMMAND=make install" \
+    --build-arg "BUILD_TAGS=netgo ledger muslc" \
+    --build-arg "COSMOS_BUILD_OPTIONS=" \
+    --build-arg "GIT_TAG=v${TAG}" \
+    --build-arg "GIT_REPO=${REPO}" \
+    --build-arg "GO_VERSION=${GO_VERSION}" \
+    --build-arg "MIMALLOC_VERSION=" \
+    --build-arg "LDFLAGS=-w -s -linkmode=external -extldflags \"-Wl,-z,muldefs -static\"" \
+    $@
