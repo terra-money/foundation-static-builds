ef7d2fc42ef76005f5e1d8e8488858e35b9e3111
gregnuj <greg@terra.money>
2023-11-01 19:11:40 -0500

    add celestia tewst build

---
+++ builds/celestia.1.3.0.alpine.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -x
+
+BASEDIR=$(dirname "$0")
+DOCKER_DIR="${BASEDIR}/.."
+IMAGE="alpine"
+NAME="celestia"
+REPO="celestiaorg/celestia-app"
+TAG="1.3.0"
+GO_VERSION="1.21.1"
+
+cd "${DOCKER_DIR}"
+docker buildx build "." -f "${IMAGE}.Dockerfile" \
+    --load \
+    --progress plain \
+    --tag "terraformlabs/${NAME}:${TAG}" \
+    --platform "linux/amd64" \
+    --build-arg "OS=linux" \
+    --build-arg "ARCH=amd64" \
+    --build-arg "APP_NAME=${NAME}" \
+    --build-arg "BIN_NAME=${NAME}-appd" \
+    --build-arg "BUILD_COMMAND=make install" \
+    --build-arg "BUILD_TAGS=netgo ledger muslc" \
+    --build-arg "CHECK_STATICALLY=false" \
+    --build-arg "COSMOS_BUILD_OPTIONS=" \
+    --build-arg "GIT_TAG=v${TAG}" \
+    --build-arg "GIT_REPO=${REPO}" \
+    --build-arg "GO_VERSION=${GO_VERSION}" \
+    --build-arg "MIMALLOC_VERSION=" \
+    --build-arg "LDFLAGS=-w -s -linkmode=external -extldflags \"-Wl,-z,muldefs -static\"" \
+    $@
